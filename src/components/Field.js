4ad53097038ee554e5e1f8d5301c1967a4c63d6c
proyev <prots.yevhen@gmail.com>
2021-07-30 23:27:16 +0200

    Add mines rendering and create field-state workflow

--- src/components/Field.js
+++ src/components/Field.js
@@ -1,14 +1,45 @@
 import React, {useState} from "react";
 
 function Field(props) {
+
+    const renderField = () => {
+
+        const cells = props.field.map((cell, index) => {
+            switch (cell) {
+                case 'O': {
+                    return (
+                        <div key={index}
+                             className="open"
+                        ></div>
+                    );
+                    break;
+                }
+                case 'OM': {
+                    return (
+                        <div key={index}
+                             className="mine"
+                        >
+                        </div>
+                    );
+                }
+                default: {
+                    return (
+                        <div key={index}
+                             className="cell"
+                             onClick={() => props.handleClick(index)}
+                        ></div>
+                    );
+                    break;
+                }
+            }
+        });
+
+        return cells;
+    }
     
     return (
         <main className="field">
-            {props.field.map((tile, index) => <div key={index}
-                                             className="cell"
-                                             onClick={props.handleClick}
-
-            >{props.field[index]}</div>)}
+            {renderField()}
         </main>
     );
 }
--- src/containers/FieldContainer.js
+++ src/containers/FieldContainer.js
@@ -1,22 +1,44 @@
-import React, {useState} from 'react';
+import React, { useState, useEffect } from 'react';
 import Field from '../components/Field';
 
 function FieldContainer() {
-    const [field, setField] = useState(Array(72).fill(null));
+    const generateField = () => {
+        const field = Array(72).fill(null);
+        for (let i = 0; i < 10; i++) {
+            const randomIndex = Math.floor(Math.random() * 72);
+            field[randomIndex] = 'M';
+        }
+        return field;
+    }
+    const [field, setField] = useState(generateField());
 
-    const handleClick = (event) => {
-        event.target.className="fired";
+    useEffect(
+        for (cell in field){
+            cell.addEventListener('')
+        }
+        , []);
+
+    const handleClick = index => {
+        const updatedField = field.slice();
+        if (updatedField[index] === 'M') {
+            console.log('YOU LOST');
+            updatedField[index] = 'OM';
+        } else {
+            updatedField[index] = 'O';
+        }
+        setField(updatedField);
     }
 
-    const handleRightClick = () => {
-
-    }
+    // const handleRightClick = () => {
+    //
+    // }
     
     return (
-        <Field handleClick={handleClick}
-               field={field}
-               
-        />
+        <>
+            <Field handleClick={handleClick}
+                   field={field}
+            />
+        </>
     );
 }
 
